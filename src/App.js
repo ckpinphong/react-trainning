9dc10d3ab1c6f049bdabc9cfd3a157e6d2aa875b
ckpinphong <ck.pinphong@gmail.com>
2020-08-09 21:28:52 +0700

    .

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           ggframo
-          khun big
+          
         </p>
         <a
           className="App-link"
